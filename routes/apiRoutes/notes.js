324455fbca7fd5019c0c8ab4233652a55c7778c6
Hiba Ali <hiba.mumin22@gmail.com>
2022-02-07 00:07:06 -0600

    added api post request, delete req

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,11 +1,38 @@
 const router = require("express").Router();
 const fs = require('fs');
+const { append } = require("vary");
 const database = require("../db/db.json");
 
 //GET request API route
+
 router.get('/notes', function(req, res) {
-  readFileAsync(./develop)
+  readFileAsync('./develop/db/db.json', 'utf-8').then(function(data) {
+      notes = [].concat(JSON.parse(data))
+      res.json(notes);
+  }); 
 });
+
+// POST request API Route
+
+router.post('/notes', function(req, res) {
+    const note = req.body;
+    readFileAsync('./develop/db/db.json', 'utf-8').then(function(data) {
+       const newNote = [].concat(JSON.parse(data));
+       note.id = newNote.length + 1 
+       newNote.push(note) 
+       return notes
+    }).then(function(notes) {
+        writeFileAsync('./develop/db/db.json', JSON.stringify(notes))
+        res.json(note);
+    })
+});
+
+// Delete Requests
+append.delete('/notes/:id', function(req, res) {
+    const deleteId = parseInt
+});
+
+
 
 
 
